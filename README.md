8cbf0c36a15c18dada384fa864c95a0d7b3616b9
alex-pythonista <indrajit.alex@gmail.com>
2024-08-15 18:43:24 +0600

    user registration and login init

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A simple event management system developed in Django
 - [x] Create events: Each event should have a title, description, date, time, and location.
 - [x] List of events: There should be an event display page
 - [x] Event details page
-- [ ] User registration, authentication
+- [x] User registration, authentication
 - [ ] User can register to an event. Note, only signed up users can register, and users should only be able to unregister from events they've registered for.
 - [ ] Keyword-based search
 - [x] Utilize Django Admin Panel
--- users/views.py
+++ users/views.py
@@ -1,24 +1,50 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.contrib.auth import authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
-from .forms import LoginForm
+from .forms import RegistrationForm
 
-def login(request):
-    form = LoginForm()
+def login_view(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            email = request.POST.get('email')
-            password = request.POST.get('password')
-            user = authenticate(request, email=email, password=password)
-            if user:
-                next_url = request.GET.get('next')
-                return redirect(next_url if next_url else 'home')
-            
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request=request, user=user)
+                messages.success(request, f"Welcome, {username}!")
+                return redirect('home')
             else:
-                form = LoginForm()
-                messages.error(request, 'Invalid email or password')
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
 
-    return render(request, 'users/login.html', {'form': form})
+    return render(request, 'users/auth.html', {'form': form, 'is_login': True})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have been logged out successfully.")
+    return redirect('login')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            messages.success(request, f"Account created for {username}!")
+            # Automatically log the user in after successful registration
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Replace 'home' with your desired redirect URL
+    else:
+        form = RegistrationForm()
+    
+    return render(request, 'users/auth.html', {'form': form})
